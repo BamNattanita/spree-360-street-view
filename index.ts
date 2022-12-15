30fccb44703ccff7d7bd8a7435720b416b312e25
Nattanita Authisanonth <nattanita@rentspree.com>
2022-12-15 07:05:08 +0700

    refactor

--- index.ts
+++ index.ts
@@ -48,34 +48,59 @@ function initMap(): void {
   '<div id="content">' +
   '<div id="siteNotice">' +
   "</div>" +
-  '<h1 id="firstHeading" class="firstHeading">Uluru</h1>' +
+  '<h1 id="firstHeading" class="firstHeading">Promphan 1</h1>' +
   '<div id="bodyContent">' +
-  "<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large " +
-  "sandstone rock formation in the southern part of the " +
-  "Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) " +
-  "south west of the nearest large town, Alice Springs; 450&#160;km " +
-  "(280&#160;mi) by road. Kata Tjuta and Uluru are the two major " +
-  "features of the Uluru - Kata Tjuta National Park. Uluru is " +
-  "sacred to the Pitjantjatjara and Yankunytjatjara, the " +
-  "Aboriginal people of the area. It has many springs, waterholes, " +
-  "rock caves and ancient paintings. Uluru is listed as a World " +
-  "Heritage Site.</p>" +
-  '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">' +
-  "https://en.wikipedia.org/w/index.php?title=Uluru</a> " +
-  "(last visited June 22, 2009).</p>" +
+  "<b>House for Rent</b>" +
+  " Tel : 02-024-6699  " +
+  " Location: Ladprao Soi 1, near MRT Ladprao " +
+  " || 5 bedrooms  " +
+  " || 5 bathrooms " +
+  " || Car Parking " +
+  " || Pets Allow " +
+  " || Price: $200000" +
   "</div>" +
   "</div>";
+
+  const contentString2 =
+  '<div id="content">' +
+  '<div id="siteNotice">' +
+  "</div>" +
+  '<h1 id="firstHeading" class="firstHeading">Baan Promphan</h1>' +
+  '<div id="bodyContent">' +
+  "<b>House for Rent</b>" +
+  " Tel : 02-024-6699  " +
+  " Location: Ladprao Soi 1, near MRT Ladprao " +
+  "|| 5 bedrooms  " +
+  "|| 5 bathrooms " +
+  "|| Car Parking " +
+  "|| Pets Allow " +
+  "|| Price: $200000" +
+  "</div>" +
+  "</div>";
+
+  const contentString1 =
+
+  '<p><a href="http://178.128.31.133:3000/">' +
+  "Click To View Virtual Home</a> " +
+  "</p>" 
+
 
 const infowindow = new google.maps.InfoWindow({
   content: contentString,
   maxWidth: 200,
-  ariaLabel: "Uluru",
+  ariaLabel: "Promphan 1",
+});
+
+const infowindowV = new google.maps.InfoWindow({
+  content: contentString1,
+  maxWidth: 200,
+  ariaLabel: "Virtual Home",
 });
 
 const infowindow2 = new google.maps.InfoWindow({
-  content: contentString,
+  content: contentString2,
   maxWidth: 200,
-  ariaLabel: "Uluru",
+  ariaLabel: "Baan Promphan",
 });
 
 const houseMarker1 = new google.maps.Marker({
@@ -116,7 +141,7 @@ const virtualMarker1 = new google.maps.Marker({
     });
 
       google.maps.event.addListener(virtualMarker1, 'click', function() {
-        infowindow.open({
+        infowindowV.open({
           anchor: virtualMarker1,
           map,
         });                      
